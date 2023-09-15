366bec368a0788fbe994fc1a552f185fff7beb63
ParagNirwan <52133765+ParagNirwan@users.noreply.github.com>
2023-09-16 00:04:39 +0200

    Login Code

--- app/src/main/java/com/shrag/mchome/login.java
+++ app/src/main/java/com/shrag/mchome/login.java
@@ -1,14 +1,75 @@
 package com.shrag.mchome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+
+    Button button;
+    EditText email,password;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.button);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+
+                if((TextUtils.isEmpty(Email))){
+                    Toast.makeText(login.this,"Please enter your email",Toast.LENGTH_SHORT);
+                }
+                else if((TextUtils.isEmpty(Password))){
+                    Toast.makeText(login.this,"Please enter your password",Toast.LENGTH_SHORT);
+                } else if (!Email.matches("^(.+)@(\\\\S+)$")) {
+                   email.setError("Invalid Email");
+                }else if(password.length()<8){
+                    password.setError("Password length too short");
+                }else{
+                    auth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                try{
+                                    Intent intent = new Intent(login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } catch (Exception e){
+                                    Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                Toast.makeText(login.this,task.getException().getMessage(),Toast.LENGTH_SHORT);
+                            }
+                        }
+                    });
+                }
+
+
+            }
+        });
     }
+
+
 }
